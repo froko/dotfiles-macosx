2704d84640975b9ca8ce7a4d3181bcf6c16fe4a8
Patrick Ineichen <github@frokonet.ch>
2023-06-25 14:47:13 +0200

    feat: 🎸 set everforest style for vim & tmux

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,16 +1,6 @@
 local wezterm = require("wezterm")
 
-local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
-custom.background = "#000000"
-custom.tab_bar.background = "#040404"
-custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
-custom.tab_bar.new_tab.bg_color = "#080808"
-
 local config = {
-  color_schemes = {
-    ["OLEDppuccin"] = custom,
-  },
-  color_scheme = "OLEDppuccin",
   window_background_opacity = 0.85,
   enable_tab_bar = false,
   window_decorations = "RESIZE",
